a38f1cc006fa91f7c99c7f185987086676ba5189
Anton Benkevich <abenkevich@alertlogic.com>
2020-06-06 19:00:18 +0100

    Add build configuration

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name='alertlogic-sdk-definitions',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    url='https://github.com/alertlogic/alertlogic-sdk-definitions',
+    license='MIT license',
+    author='Alert Logic Inc.',
+    author_email='devsupport@alertlogic.com',
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    description='The Alert Logic API definitions.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    entry_points = {
+        'console_scripts': [
+            'alcli = alcli.alertlogic_cli:main'
+        ]
+    },
+    scripts=[],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'troubleshooting']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=requirements,
+    extras_require={
+        'dev': [
+            'pytest>=3',
+            'mock>=2.0.0',
+            'httpretty>=0.8.14',
+            'pycodestyle>=2.3.1'
+        ],
+    },
+    keywords=['alcli', 'almdr', 'alsdkdefs', 'alertlogic', 'alertlogic-cli']
+)
