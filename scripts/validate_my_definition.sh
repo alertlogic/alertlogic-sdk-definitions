a9d09aa03ebff0ce1777041b500210f842097d95
Anton B <anton.benkevich@gmail.com>
2020-06-28 10:54:00 +0100

    Use validation script at master (#11)
    Co-authored-by: Anton Benkevich <abenkevich@alertlogic.com>

--- scripts/validate_my_definition.sh
+++ scripts/validate_my_definition.sh
@@ -6,6 +6,11 @@ export TEMP=$(mktemp -d -t alertlogic-sdk-definitions-validation-XXXX)
 if python3 -m venv $TEMP; then
   source $TEMP/bin/activate
   pip3 install requests jsonschema PyYaml
-  curl https://raw.githubusercontent.com/alertlogic/alertlogic-sdk-definitions/validation-script/scripts/validate_my_definition.py -o $TEMP/validate_my_definition.py
-  python3 $TEMP/validate_my_definition.py -f $1 && rm -rf $TEMP
+  curl https://raw.githubusercontent.com/alertlogic/alertlogic-sdk-definitions/master/scripts/validate_my_definition.py -o $TEMP/validate_my_definition.py
+  if [ $# -eq 0 ]
+  then
+    python3 $TEMP/validate_my_definition.py && rm -rf $TEMP
+  else
+    python3 $TEMP/validate_my_definition.py -d $1 && rm -rf $TEMP
+  fi
 fi
