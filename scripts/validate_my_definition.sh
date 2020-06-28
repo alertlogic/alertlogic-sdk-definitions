f593c178c0d6515fafeecc6157cc5ebdd48ff809
Anton Benkevich <abenkevich@alertlogic.com>
2020-06-28 11:16:26 +0100

    Look for python interpreter

--- scripts/validate_my_definition.sh
+++ scripts/validate_my_definition.sh
@@ -3,14 +3,25 @@ set +e
 
 export TEMP=$(mktemp -d -t alertlogic-sdk-definitions-validation-XXXX)
 
-if python3 -m venv $TEMP; then
+if command -v python3.8; then
+  PYTHON=python3.8
+elif command -v python3.7; then
+  PYTHON=python3.7
+elif command -v python3.6; then
+  PYTHON=python3.6
+else
+  echo "No suitable python3 interpreter is found"
+  exit 1
+fi
+
+if $PYTHON -m venv $TEMP; then
   source $TEMP/bin/activate
   pip3 install requests jsonschema PyYaml
   curl https://raw.githubusercontent.com/alertlogic/alertlogic-sdk-definitions/master/scripts/validate_my_definition.py -o $TEMP/validate_my_definition.py
   if [ $# -eq 0 ]
   then
-    python3 $TEMP/validate_my_definition.py && rm -rf $TEMP
+    $PYTHON $TEMP/validate_my_definition.py && rm -rf $TEMP
   else
-    python3 $TEMP/validate_my_definition.py -d $1 && rm -rf $TEMP
+    $PYTHON $TEMP/validate_my_definition.py -d $1 && rm -rf $TEMP
   fi
 fi
