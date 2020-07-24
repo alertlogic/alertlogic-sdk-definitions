abd12117ee86e4f8dda81e8f9894bb92e74db786
Anton B <anton.benkevich@gmail.com>
2020-07-24 10:15:10 +0100

    Move parsing, loading and normalisation logic for the definitions to the alsdkdefs package (#92)
    Add common validation point
    Improve validation
    
    Co-authored-by: Anton Benkevich <abenkevich@AL-CAR-09946.local>

--- tests/test_apis_validate.py
+++ tests/test_apis_validate.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python
 """Tests for `alertlogic-sdk-definitions` package."""
 
-
-import requests
-import jsonschema
-import yaml
 import unittest
 import alsdkdefs
-from almdrlib.client import _YamlOrderedLoader
-
-OPENAPI_SCHEMA_URL = 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/schemas/v3.0/schema.json'
 
 
 class TestServiceDefs(unittest.TestCase):
-
     def setUp(self):
         """Setup"""
-        r = requests.get(OPENAPI_SCHEMA_URL)
-        self.schema = r.json()
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
@@ -26,9 +16,7 @@ class TestServiceDefs(unittest.TestCase):
         services = alsdkdefs.list_services()
         for service in services:
             print("Validating ", service)
-            for definition in alsdkdefs.get_service_defs(service):
-                print("Validating def", definition)
-                with open(definition, 'r') as f:
-                    spec = f.read()
-                    obj = yaml.load(spec, Loader=_YamlOrderedLoader)
-                    jsonschema.validate(obj, self.schema)
+            for defintion_file in alsdkdefs.get_service_defs(service):
+                print("Validating def", defintion_file)
+                obj = alsdkdefs.get_spec(defintion_file)
+                alsdkdefs.validate(obj, defintion_file)
