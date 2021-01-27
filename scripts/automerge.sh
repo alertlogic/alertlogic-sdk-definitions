05a738d3e529793b8809f54b44db72da6ee6918c
Anton Benkevich <abenkevich@alertlogic.com>
2021-01-27 13:00:09 +0000

    exclude auto branches from test all fix automerge

--- scripts/automerge.sh
+++ scripts/automerge.sh
@@ -34,15 +34,17 @@ git fetch origin $GITHUB_HEAD_REF:$GITHUB_HEAD_REF
 printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
 git checkout "$BRANCH_TO_MERGE_INTO"
 
-MERGE_COMMIT_MESSAGE=`git show-branch --no-name $GITHUB_HEAD_REF`
+MERGE_COMMIT_MESSAGE=$(git show-branch --no-name $GITHUB_HEAD_REF)
 
 printf 'Merging %s using "%s" message for if merge commit\n' "$GITHUB_HEAD_REF" "${MERGE_COMMIT_MESSAGE}" >&2
-git merge "$GITHUB_HEAD_REF" -m '${MERGE_COMMIT_MESSAGE}'
+git merge "$GITHUB_HEAD_REF" -m "${MERGE_COMMIT_MESSAGE}"
 
 printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
 
 push_uri="https://$GITHUB_TOKEN@github.com/$GITHUB_REPO"
 
 # Redirect to /dev/null to avoid secret leakage
-git push "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
-git push "$push_uri" :"$GITHUB_HEAD_REF" >/dev/null 2>&1
+#git push "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
+git push "$push_uri" "$BRANCH_TO_MERGE_INTO"
+#git push "$push_uri" :"$GITHUB_HEAD_REF" >/dev/null 2>&1
+git push "$push_uri" :"$GITHUB_HEAD_REF"
