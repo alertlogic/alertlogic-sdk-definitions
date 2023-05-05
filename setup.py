1af017d7d2c7f51bc3a626f557e88a6abc926358
Evan Brown <evan.brown@alertlogic.com>
2023-05-05 12:05:38 -0400

    update setup.py with urllib3 pin

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = ['jsonschema[format_nongpl]==3.2.0', 'pyyaml>=5.4.1', 'requests>=2.18', 'jsonmerge>=1.8.0']
+requirements = ['jsonschema[format_nongpl]==3.2.0', 'pyyaml>=5.4.1', 'requests>=2.18', 'urllib3<2', 'jsonmerge>=1.8.0']
 
 setup(
     name='alertlogic-sdk-definitions',
