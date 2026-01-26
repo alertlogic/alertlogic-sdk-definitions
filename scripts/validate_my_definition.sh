26b464aeb4a3be56a8e51c8834b1b5470af1b0e8
Gennady Proskurin <gpr@mail.ru>
2026-01-26 11:39:04 +0000

    Allow python interpreter to be specified in env variable

--- scripts/validate_my_definition.sh
+++ scripts/validate_my_definition.sh
@@ -3,7 +3,9 @@ set -e
 
 export TEMP=$(mktemp -d -t alertlogic-sdk-definitions-validation-XXXX)
 
-if command -v python3.10; then
+if [ -n "$AL_SDK_VALIDATE_PYTHON" ]; then
+  PYTHON="$AL_SDK_VALIDATE_PYTHON"
+elif command -v python3.10; then
   PYTHON=python3.10
 elif command -v python3.9; then
   PYTHON=python3.9
@@ -20,6 +22,8 @@ else
   exit 1
 fi
 
+echo "OpenAPI validation using python: $PYTHON"
+$PYTHON --version
 $PYTHON -m venv $TEMP
 source $TEMP/bin/activate
 pip3 install alertlogic-sdk-definitions --no-cache-dir --ignore-requires-python
